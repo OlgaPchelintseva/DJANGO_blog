a6d607e5af199c31f040011e37a99ba8874a36bd
Start Soft <StartSoft@ItTopNSK.local>
2026-09-02 21:11:19 +0700

    fix: userAcc profile

--- userProfile/forms.py
+++ userProfile/forms.py
@@ -3,18 +3,18 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailInput(
+    email = forms.EmailField(
         required = True,
         widget = forms.EmailInput(
         attrs = {
             'class': 'form-input',
             'placeholder': 'Введите Email'
-        }),
+    }),
         label = 'Электронная почта'
     )
     class Meta:
         model = User
-        fields = ['userame', 'email']
+        fields = ['username', 'email']
         widgets = {
             'username': forms.TextInput(attrs={
                 'class': 'form-input',
--- userProfile/middleware.py
+++ userProfile/middleware.py
@@ -12,12 +12,12 @@ class BlockCheckMiddleware:
         if request.user.is_authenticated:
             profile = getattr(request.user, 'profile', None)
             if profile and profile.is_curently_blocked():
-                if profile.blocked_untill:
-                    date_str = profile.blocked_untill.strftime('%d.%m.%Y %H:%i')
+                if profile.blocked_until:
+                    date_str = profile.blocked_until.strftime('%d.%m.%Y %H:%M')
                     messages.error(request, f'Ваш аккаунт заблокирован до {date_str}')
                 else:
                     messages.error(request, f'Ваш аккаунт заблокирован навсегда')
                 logout(request)
                 return redirect('login')
-            return self.get_response(request)
+        return self.get_response(request)
                 
